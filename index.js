2645ad84a3ab711df9a553935c7b187db78ae2a9
Nancy D <nancydo7@gmail.com>
2018-05-15 17:38:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ function destructivelyAppendKitten(name){
   
 }
 function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
   
 }
 function destructivelyRemoveLastKitten(){
